1e63868e130c3aace5056ac255a13949179dcc6e
yuu-256 <ezayuu12@icloud.com>
2024-10-18 17:09:04 +0900

    OpenData.f90 iug_n, iug_b -> iug

--- LBIO/OpenData.f90
+++ LBIO/OpenData.f90
@@ -1,18 +1,21 @@
-subroutine OpenData(iua,ius,iuk,iup,iukd,iug_n,iug_b,iuo,iuow)
+subroutine OpenData(icont,iua,ius,iuk,iup,iukd,iug_n,iug_b,iuo,iuow)
     !!! OPEN DATA FILES
 
-    !!! DESCRIPTION:
+    !!! DECLARATION:
     use params
     implicit none
-    integer, intent(out) :: iua,ius,iuk,iup,iukd,iug_n,iug_b,iuo,iuow
-    character(len=80) :: fnm 
+    integer, intent(in) :: icont(10)
+    integer, intent(out) :: iua,ius,iuk,iup,iukd,iug,iuo,iuow
+    character(len=80) :: fnm
+    integer :: ngas(2)
 
     ! data file i/o units
     iua= 3    ! atmospheric model library read
-    iug_n= 8 ! CKD gas absorption table for narrow band
-    iug_b= 9 ! CKD gas absorption table for broad band
+    iug= 7 ! CKD gas absorption table for narrow band
     iuk=11    ! Mie kernel file
     ius=12    ! Aerosol library
+    iup=13    ! photon scattering phase function
+    iukd=14
 
     ! work file i/o units
     iuo=77
@@ -33,11 +36,17 @@ subroutine OpenData(iua,ius,iuk,iup,iukd,iug_n,iug_b,iuo,iuow)
        recl=4*knang2*kpol2)
     fnm='01MCD1/dkrnl/PKRNL.OUT_asp207'
     open(iukd,file=fnm,status='old',form='unformatted')
-    fnm='01MCD1/PARAG.ch111'         ! CKD gas absorption table
-    open(iug_b,file=fnm,status='old')
-    fnm='01MCD1/ckd.g.ch_2_2e3_ltl'         ! CKD gas absorption table
-    open(iug_n,file=fnm,status='old',access='direct',form='unformatted', &
-         recl=4*kp*kt*ngas(1)*kmol1)
+
+    if(icont(2)<=1) then
+       fnm='01MCD1/PARAG.ch111'         ! CKD gas absorption table
+       open(iug,file=fnm,status='old')
+    else if(icont(2)==2) then
+       ngas(1:2)=(/2,2000/)
+       fnm='01MCD1/ckd.g.ch_2_2e3_ltl'     ! CKD gas absorption table
+       open(iug,file=fnm,status='old',access='direct',form='unformatted', &
+            recl=4*kp*kt*ngas(1)*kmol1)
+    end if
+
     open (iuo,file='out' )           ! standard output file
     open(iuow,file='outw')           ! work file for photon info
 
